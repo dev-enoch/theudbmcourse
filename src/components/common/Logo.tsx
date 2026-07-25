ac2ea56aec522fd622eb940bda26b12f1396b699
Dev Enoch <arhyelphilip024@gmail.com>
2026-07-25 14:36:11 +0100

    feat: add favicons and update logo

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,23 +1,14 @@
 import { cn } from '@/lib/utils';
-import type { SVGProps } from 'react';
+import Image from 'next/image';
 
-export function Logo(props: SVGProps<SVGSVGElement>) {
+export function Logo({ className }: { className?: string }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={cn('h-6 w-6 text-primary', props.className)}
-      {...props}
-    >
-      <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z" />
-      <polyline points="14 2 14 8 20 8" />
-      <path d="m10 14-2 2 2 2" />
-      <path d="m14 18 2-2-2-2" />
-    </svg>
+    <Image
+      src="/logo.png"
+      alt="Logo"
+      width={256}
+      height={256}
+      className={cn('h-8 w-auto', className)}
+    />
   );
 }
